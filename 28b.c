72d1a4e001fbed4c2888d6633e1e9e073f6142a5
taRUSH24 <tarush1510singh@gmail.com>
2025-09-22 21:35:00 +0530

    Update 28b.c

--- 28b.c
+++ 28b.c
@@ -1,1 +1,23 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n); // size le lo
+
+    int arr[n]; // array bana diya
+
+    // input array ke elements
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    // print array
+    for (int i = 0; i < n; i++)
+    {
+        printf("%d ", arr[i]);
+    }
+
+    return 0;
+}
