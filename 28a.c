ccc9bf187164327e8065d6c93bc7e1ade06cfb5c
taRUSH24 <tarush1510singh@gmail.com>
2025-09-22 21:33:31 +0530

    Update 28a.c

--- 28a.c
+++ 28a.c
@@ -1,1 +1,28 @@
-#include<stdio.h>
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d", &n); // input le lo
+
+    for (int num = 2; num <= n; num++) // 2 se n tak check karenge
+    {
+        int isPrime = 1; // assume karlo prime hai
+
+        for (int i = 2; i <= num / 2; i++) // divide karke check karte hain
+        {
+            if (num % i == 0) // agar divide ho gaya to prime nahi hai
+            {
+                isPrime = 0;
+                break;
+            }
+        }
+
+        if (isPrime == 1) // prime nikla
+        {
+            printf("%d ", num);
+        }
+    }
+
+    return 0;
+}
