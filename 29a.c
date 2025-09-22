cc2ca6309d58442b80a6894811dd6c28877d73bd
taRUSH24 <tarush1510singh@gmail.com>
2025-09-22 21:36:09 +0530

    Update 29a.c

--- 29a.c
+++ 29a.c
@@ -1,4 +1,21 @@
 #include <stdio.h>
+
 int main()
 {
+    int n;
+    scanf("%d", &n); // size of array
+
+    int arr[n];
+    int sum = 0; // yaha par total rakhenge
+
+    // input + sum calculation ek hi sath kar lo
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+        sum = sum + arr[i]; // har element add karte jao
+    }
+
+    printf("%d\n", sum); // final answer
+
+    return 0;
 }
