3db12d7bb767cedb0387217f00609d5bc0a44f39
taRUSH24 <tarush1510singh@gmail.com>
2025-09-22 21:37:17 +0530

    Update 29b.c

--- 29b.c
+++ 29b.c
@@ -1,4 +1,33 @@
 #include <stdio.h>
+
 int main()
 {
+    int n;
+    scanf("%d", &n); // size of array
+
+    int arr[n];
+    // array input
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+
+    int max = arr[0]; // assume first is max
+    int min = arr[0]; // assume first is min
+
+    for (int i = 1; i < n; i++) // check baaki elements
+    {
+        if (arr[i] > max)
+        {
+            max = arr[i];
+        }
+        if (arr[i] < min)
+        {
+            min = arr[i];
+        }
+    }
+
+    printf("Max=%d, Min=%d\n", max, min);
+
+    return 0;
 }
