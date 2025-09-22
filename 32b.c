60bc41ea11a841900e4fa6ecac597b2021883af0
taRUSH24 <tarush1510singh@gmail.com>
2025-09-22 21:47:06 +0530

    Update 32b.c

--- 32b.c
+++ 32b.c
@@ -1,1 +1,33 @@
 #include <stdio.h>
+
+int main()
+{
+    long long num;
+    scanf("%lld", &num); // number input
+
+    int count[10] = {0}; // digit frequency array
+
+    // break number into digits
+    while (num > 0)
+    {
+        int digit = num % 10; // last digit
+        count[digit]++;       // increase frequency
+        num = num / 10;       // remove last digit
+    }
+
+    int maxDigit = 0;
+    int maxCount = count[0];
+
+    for (int i = 1; i < 10; i++) // check which digit has max frequency
+    {
+        if (count[i] > maxCount)
+        {
+            maxCount = count[i];
+            maxDigit = i;
+        }
+    }
+
+    printf("%d\n", maxDigit);
+
+    return 0;
+}
