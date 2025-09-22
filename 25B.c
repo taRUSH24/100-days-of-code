c8b37446beed01e084411168a66c5bb352797822
taRUSH24 <tarush1510singh@gmail.com>
2025-09-22 21:07:50 +0530

    Update 25B.c

--- 25B.c
+++ 25B.c
@@ -1,1 +1,22 @@
-stdio.h
+#include <stdio.h>
+
+int main()
+{
+    // 5 rows ka pattern banana hai
+    for (int i = 0; i < 5; i++) // i row number hai
+    {
+        // pehle space print hoga
+        for (int s = 0; s < i; s++) // jitna row number utne space
+        {
+            printf(" ");
+        }
+        // ab stars print karte hain
+        for (int j = 0; j < 5 - i; j++) // har row me stars ghatte jayenge
+        {
+            printf("*");
+        }
+        printf("\n"); // row khatam to new line
+    }
+
+    return 0;
+}
