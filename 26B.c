1a61bbe36ad45633f6a1a8c88964322185252927
taRUSH24 <tarush1510singh@gmail.com>
2025-09-22 21:22:56 +0530

    Update 26B.c

--- 26B.c
+++ 26B.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
-int main(){
+
+int main()
+{
+    int stars[] = {1, 3, 5, 3, 1}; // kitne stars har block me
+    int n = 5; // total blocks
+
+    for (int i = 0; i < n; i++) // har block ke liye loop
+    {
+        for (int j = 0; j < stars[i]; j++) // block ke andar stars print karo
+        {
+            printf("*\n");
+        }
+        if (i != n - 1) // last block ke baad blank line nahi chahiye
+        {
+            printf("\n");
+        }
+    }
+
+    return 0;
 }
