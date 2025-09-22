a9962cd178e9948b833cd05d0a94b6b5b12d00ac
taRUSH24 <tarush1510singh@gmail.com>
2025-09-22 21:31:11 +0530

    Update 27a.c

--- 27a.c
+++ 27a.c
@@ -1,3 +1,30 @@
-#include<stdio.h>
-int main(){
+#include <stdio.h>
+
+int main()
+{
+    int n = 5; // peak line (yaha tak badhega)
+
+    // upar ka part (1 se odd numbers badhte hue)
+    for (int i = 1; i <= n; i++)
+    {
+        int stars = 2 * i - 1; // formula for odd numbers 1,3,5,...
+        for (int j = 1; j <= stars; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // niche ka part (reverse)
+    for (int i = n - 1; i >= 1; i--)
+    {
+        int stars = 2 * i - 1;
+        for (int j = 1; j <= stars; j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
