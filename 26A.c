fe09a6dfc994b8680e53dc26b39fee86db52a5cc
taRUSH24 <tarush1510singh@gmail.com>
2025-09-22 21:08:51 +0530

    Update 26A.c

--- 26A.c
+++ 26A.c
@@ -1,9 +1,16 @@
-#include <stdio.h)
-int main(){
+#include <stdio.h>
 
+int main()
+{
+    // har row ka start number alag hai aur end hamesha 5 hai
+    for (int i = 5; i >= 1; i--) // row decide karega kaha se start karna hai
+    {
+        for (int j = i; j <= 5; j++) // row ka start se lekar 5 tak print karo
+        {
+            printf("%d", j);
+        }
+        printf("\n"); // row complete to naya line
+    }
 
-
-
-
-
+    return 0;
 }
