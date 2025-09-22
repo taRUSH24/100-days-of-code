fdb0e0a5941bab7d79768df20a4b81ad60cfce1e
taRUSH24 <tarush1510singh@gmail.com>
2025-09-22 21:39:10 +0530

    Update 30a.c

--- 30a.c
+++ 30a.c
@@ -1,4 +1,28 @@
-#include<stdio.h>
-int main() 
+#include <stdio.h>
+
+int main()
 {
+    int n;
+    scanf("%d", &n); // size of array
+
+    int arr[n];
+    int even = 0, odd = 0; // counters
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+
+        if (arr[i] % 2 == 0) // check even
+        {
+            even++;
+        }
+        else // otherwise odd
+        {
+            odd++;
+        }
+    }
+
+    printf("Even=%d, Odd=%d\n", even, odd);
+
+    return 0;
 }
