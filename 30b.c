5deb02d8c157b453abef2dc3b59478ccf172f91d
taRUSH24 <tarush1510singh@gmail.com>
2025-09-22 21:40:44 +0530

    Update 30b.c

--- 30b.c
+++ 30b.c
@@ -1,4 +1,32 @@
-#include<stdio.h>
+#include <stdio.h>
+
 int main()
 {
+    int n;
+    scanf("%d", &n); // size of array
+
+    int arr[n];
+    int pos = 0, neg = 0, zero = 0; // counters
+
+    for (int i = 0; i < n; i++)
+    {
+        scanf("%d", &arr[i]);
+
+        if (arr[i] > 0) // positive check
+        {
+            pos++;
+        }
+        else if (arr[i] < 0) // negative check
+        {
+            neg++;
+        }
+        else // means zero
+        {
+            zero++;
+        }
+    }
+
+    printf("Positive=%d, Negative=%d, Zero=%d\n", pos, neg, zero);
+
+    return 0;
 }
