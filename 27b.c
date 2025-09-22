79f65413fdfd7d7dfddb2bcd9bf8f4d906d2b8ae
taRUSH24 <tarush1510singh@gmail.com>
2025-09-22 21:32:30 +0530

    Update 27b.c

--- 27b.c
+++ 27b.c
@@ -1,4 +1,40 @@
 #include <stdio.h>
+
 int main()
 {
+    int n = 4; // yaha tak top half jayega
+
+    // top half
+    for (int i = 1; i <= n; i++)
+    {
+        // spaces first
+        for (int s = 1; s <= n - i; s++)
+        {
+            printf(" ");
+        }
+        // stars after that
+        for (int j = 1; j <= (2 * i - 1); j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // bottom half
+    for (int i = n - 1; i >= 1; i--)
+    {
+        // spaces
+        for (int s = 1; s <= n - i; s++)
+        {
+            printf(" ");
+        }
+        // stars
+        for (int j = 1; j <= (2 * i - 1); j++)
+        {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    return 0;
 }
